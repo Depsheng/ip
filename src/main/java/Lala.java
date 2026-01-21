8c0aa716cf23c70adbc7e6be58d104994c8d0034
Depsheng <jiesheng.yeo@gmail.com>
2026-01-21 12:31:11 +0800

    Level-0
    Changed bot name to Lala and changed text output

---
+++ src/main/java/Lala.java
@@ -0,0 +1,6 @@
+public class Lala {
+    public static void main(String[] args) {
+        Welcome.printWelcome();
+        Bye.printBye();
+    }
+}
---
+++ src/main/java/Welcome.java
@@ -0,0 +1,8 @@
+public class Welcome {
+    public static void printWelcome() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Lala");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+    }
+}
