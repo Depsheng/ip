d434762f6ec32646ad92c1b8428afc4e285461ff
Depsheng <jiesheng.yeo@gmail.com>
2026-01-29 12:15:03 +0800

    add Ui class
    added Ui class to handle all UI commands and also made the Lala class more OOP

--- src/main/java/Bye.java
+++
@@ -1,7 +0,0 @@
-public class Bye{
-    public static void printBye() { // Print Bye message and exit Lala
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
-    }
-}
---
+++ src/main/java/Ui.java
@@ -0,0 +1,44 @@
+public class Ui {
+    private static final String LINE = "____________________________________________________________";
+
+    public void showWelcome() {
+        System.out.println(LINE);
+        System.out.println("Hello! I'm Lala");
+        System.out.println("What can I do for you?");
+        System.out.println(LINE);
+    }
+
+    public void showBye() {
+        System.out.println(LINE);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+    }
+
+    public void showLine() {
+        System.out.println(LINE);
+    }
+
+    public void showErrorUnknownCommand() {
+        System.out.println(LINE);
+        System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means.");
+        System.out.println(LINE);
+    }
+
+    public void showErrorEmptyDescription() {
+        System.out.println(LINE);
+        System.out.println("☹ OOPS!!! The description of a task cannot be empty.");
+        System.out.println(LINE);
+    }
+
+    public void showErrorGeneric(String msg) {
+        System.out.println(LINE);
+        System.out.println("☹ OOPS!!! Something went wrong: " + msg);
+        System.out.println(LINE);
+    }
+
+    public void showMessage(String msg) {
+        System.out.println(LINE);
+        System.out.println(msg);
+        System.out.println(LINE);
+    }
+}
